11b069b369261aeab6159b2711c3bc394adebae4
AleksandarBorisov <a_borisov@abv.bg>
2018-04-03 15:23:38 +0300

    Commiting the tasks of the first assignment

---
+++ C# Basics/1. Introduction To Programming/Blank solution/Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Math.Sqrt(12345));
+        }
+    }
+}
---
+++ C# Basics/1. Introduction To Programming/Добър Ден!/Tasks/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("1");
+            Console.WriteLine("101");
+            Console.WriteLine("1001");
+        }
+    }
+}
