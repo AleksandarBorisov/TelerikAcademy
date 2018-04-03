6b2c395cccce647db63e67e36d8291a3758f6422
AleksandarBorisov <a_borisov@abv.bg>
2018-04-03 22:03:35 +0300

    Submitting third assignment

---
+++ C# Basics/3. Operators and expressions/Modul3/reactangle/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace reactangle
+{
+    class Program
+    {
+        static void Main()
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:0.00} {1:0.00}", width * height, 2 * width + 2 * height);
+        }
+    }
+}
