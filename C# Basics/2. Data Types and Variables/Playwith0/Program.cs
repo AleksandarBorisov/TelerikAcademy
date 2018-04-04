5273dc5e3ff6b845dc2f8ff8da2cfda09e4995f8
AleksandarBorisov <a_borisov@abv.bg>
2018-04-05 01:01:49 +0300

    Final touches Second assignment

---
+++ C# Basics/2. Data Types and Variables/Playwith0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playwith0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? zeroint = null;
+            double? zerodouble = null;
+            zeroint = 5;
+            Console.WriteLine(zeroint+5);
+            Console.WriteLine(zerodouble+1.3);
+        }
+    }
+}
---
+++ C# Basics/2. Data Types and Variables/Quotations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Quotations
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(@"The ""use"" of quotations causes difficulties.");
+        }
+    }
+}
