09da7d5ae2d37f2eacb10f775a6c5f5af168a5da
AleksandarBorisov <a_borisov@abv.bg>
2018-04-05 12:54:35 +0300

    This is the new folder for the third assignment

---
+++ C# Basics/3. Operators and Expressions/Modul3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose Option (1/2): ");
+            var key = Console.ReadKey().KeyChar;
+            switch (key)
+            {
+                case "1":
+                    {
+                        Console.Write("Моля въведете число: ");
+                        int n = int.Parse(Console.ReadLine());
+                        n = n % 2;
+                        Console.WriteLine(n == 0 ? "Числото е четно" : "Числото е нечетно");
+                    }
+                case "2":
+                    {
+                        Console.Write("Моля въведете число: ");
+                        int number = int.Parse(Console.ReadLine());
+                        int remainderFive = number % 5;
+                        int remainderSeven = number % 7;
+                        Console.WriteLine(remainderFive == 0 && remainderSeven == 0 ? "Числото се дели без остатък" : "Числото не се дели без остатък");
+                    }
+            }
+        }
+    }
+}
---
+++ C# Basics/3. Operators and Expressions/Trapezoid/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Trapezoid
+{
+    class Program
+    {
+        static void Main()
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:0.0000000}", ((a + b)/2) * h);
+        }
+    }
+}
