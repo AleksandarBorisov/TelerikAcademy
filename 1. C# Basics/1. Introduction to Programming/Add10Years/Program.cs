0e452db07bfbbd1604e682a843998a1ba66a3c37
AleksandarBorisov <a_borisov@abv.bg>
2018-04-05 14:10:27 +0300

    Submitting first assignment

---
+++ 1. C# Basics/1. Introduction to Programming/Add10Years/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class HelloCSharp
+    {
+        static void Main()
+        {
+            DateTime birthDay;
+            while (!DateTime.TryParse(Console.ReadLine(), out birthDay))
+            {
+                Console.WriteLine("Моля въведете коректна дата: ");
+            }
+            int difference = (birthDay.DayOfYear > DateTime.Now.DayOfYear) ? (-birthDay.Year - 1) : (-birthDay.Year);
+            DateTime age = DateTime.Now.AddYears(difference);
+            Console.WriteLine(age.Year);
+            Console.WriteLine(age.AddYears(10).Year);
+        }
+    }
+}
